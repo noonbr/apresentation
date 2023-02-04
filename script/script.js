5cd867e4ba672818d008fa6a20d4e21bf728055c
Non <nonpl4ys@gmail.com>
2023-02-04 20:28:52 -0300

    Added light-mode and some corrections

--- script/script.js
+++ script/script.js
@@ -1,11 +1,26 @@
-function copyDiscord() {
-    const discord = document.getElementById("discord-js-text")
+var discordButton = document.querySelector(".discord")
+discordButton.onclick = function() {
+    var discordSpan = document.getElementById("discord-js-text")
     navigator.clipboard.writeText("non#8832")
-    discord.innerText = "Copiado"
-    setTimeout(
-        function (){
-            discord.innerHTML = "Discord"
-        },
-        1000
-    )
+    discordSpan.textContent = "Copiado"
+    setTimeout(() => {
+        discordSpan.textContent = "Discord"
+    }, 1000)
+}
+
+var button = document.getElementById("light-mode")
+console.log(button);
+button.onclick = function(){
+    var body = document.querySelector("body");
+
+    if (body.classList.contains("light-mode")){
+        body.classList.remove("light-mode");
+        this.classList.remove("fa-moon");
+        this.classList.add("fa-sun");
+        
+    } else {
+        body.classList.add("light-mode");
+        this.classList.remove("fa-sun");
+        this.classList.add("fa-moon");
+    }
 }
